e5cc61ff6507b4a7b9b9bdd16120dcac49f26570
T4910 <taiwo.emmanuel@lmu.edu.ng>
2024-03-06 17:30:45 +0100

    [FIXED] foodlist css problem

--- app/(routes)/(home)/_components/results.jsx
+++ app/(routes)/(home)/_components/results.jsx
@@ -2,14 +2,16 @@ import Card from "./foodcard"
 
 const results = ({results}) => {
   const number = results.length;
-
-  const heightLogic = (number !== 1) ? // Displays in a certain way if just one item
-                        (number === 2) ? 
-                          `h-[77%] content-evenly` : // tailwind calc can't calculate negative number multiplication, so had to add this login here
-                          `${`h-[calc(100%+(${number - ((number % 2 === 0) ? 3 : 2)}*23%))] content-evenly`}` // The calc function below calculates the height required of the div to allow the cards to only wrap twice 
-                      : 'ml-2.5';
-  return (
-      <div className={`${heightLogic} pt-48 flex flex-col flex-wrap space-y-4 `}>
+  const spacing = (number !== 1) ? 'content-evenly' : 'ml-2.5';
+  
+  // The calc function below calculates the height required of the div 
+  // to allow the cards to only wrap once i.e 2 spaces for cards 
+  let height = {
+    height: `calc(100% + (${number - ((number % 2 === 0) ? 3 : 2)} * 23%))`
+  };
+  
+    return (
+      <div className={`${spacing} pt-48 flex flex-col flex-wrap space-y-4`} style={height}>
           <h3 className="font-bold text-2xl inline w-44 text-left pl-4">
             Found <br /> {number} results
           </h3>
@@ -27,7 +29,7 @@ const results = ({results}) => {
               />)
           }
       </div>
-  )
-}
+  );
+};
 
-export default results
+export default results;
