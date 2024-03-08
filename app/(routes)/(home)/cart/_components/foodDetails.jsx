63e77dfcf76423008eed4265289e38e514c98596
T4910 <taiwo.emmanuel@lmu.edu.ng>
2024-03-08 20:29:22 +0100

    [FIX] adjusted counter size in food card in cart page

--- app/(routes)/(home)/cart/_components/foodDetails.jsx
+++ app/(routes)/(home)/cart/_components/foodDetails.jsx
@@ -3,7 +3,7 @@ import Price from '../../_components/price'
 
 const foodDetails = ({ name, description, price, number }) => {
     return (
-        <div className='pl-2 flex flex-col h-full py-1'>
+        <div className='pl-2 flex flex-col h-full py-1 gap-1'>
             <div className='flex-grow'>
                 <h3 className='w-fit font-medium text-lg'>{name}</h3>
                 <p className='w-44 text-wrap text-muted-foreground'>{description}</p>
@@ -13,7 +13,7 @@ const foodDetails = ({ name, description, price, number }) => {
                 <Counter 
                     number={number}
                     bodyStyle="bottom-0 text-md font-medium"
-                    btnStyle="py-1 px-3 text-lg"
+                    btnStyle="py-0 px-3 text-lg"
                     iconStyle="size-4"
                     numStyle="text-lg font-semibold px-0.5"
                 />
